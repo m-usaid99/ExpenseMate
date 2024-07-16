3dec783a7c6e27f4ae870123d7523ef4c526d3d0
Muhammad Usaid Rehman <rehman.usaid@gmail.com>
2024-07-17 04:55:39 +0500

    refactored layout for dashboard

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,29 +1,32 @@
 // src/pages/Dashboard.js
-import React from 'react';
-import { Box, Container, Typography } from '@mui/material';
-import SummaryCardsSection from '../components/SummaryCardsSection';
-import ChartsSection from '../components/ChartsSection';
-import RecentTransactions from '../components/RecentTransactions';
-import NavBar from '../components/NavBar';
-import Sidebar from '../components/Sidebar'
+import React from "react";
+import { Container, Typography } from "@mui/material";
+import SummaryCardsSection from "../components/SummaryCardsSection";
+import ChartsSection from "../components/ChartsSection";
+import RecentTransactions from "../components/RecentTransactions";
+import Layout from "../components/Layout";
 
 const Dashboard = () => {
   return (
-    <Box sx={{ display: 'flex', p: 2}}>
-      <NavBar />
-      <Sidebar />
-
-      <Container maxWidth="lg" sx={{ flexGrow: 1, p: 4, paddingTop: 6 }}>
-        <Box sx={{ mt: 4 }}>
-          <Typography variant="h4" component="h1" gutterBottom>
+    <Layout>
+        <Container
+          maxWidth="lg"
+          sx={{
+            paddingTop: 4,
+            paddingBottom: 4,
+            display: "flex",
+            flexDirection: "column",
+            marginLeft: 0,
+          }}
+        >
+          <Typography variant="h4" gutterBottom>
             Dashboard
           </Typography>
           <SummaryCardsSection />
           <ChartsSection />
           <RecentTransactions />
-        </Box>
-      </Container>
-    </Box>
+        </Container>
+    </Layout>
   );
 };
 
