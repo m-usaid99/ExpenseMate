172720d11ccaa08dd5b9ef2223babe35f8dd4e21
Muhammad Usaid Rehman <rehman.usaid@gmail.com>
2024-07-17 04:00:09 +0500

    removed src from root

--- src/components/charts/BudgetTrackingChart.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const BudgetTrackingChart = () => {
-    return (
-        <>Budget chart</>
-    )
-};
-
-export default BudgetTrackingChart;
--- src/pages/ProfilePage.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const ProfilePage = () => {
- return (
-    <>Profile</>
- )
-}
-
-export default ProfilePage;
